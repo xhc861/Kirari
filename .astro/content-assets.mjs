c0f8c2cb7c67735cb80fa7d0470dbf6b77833fc6
xhc861 <3043536727@qq.com>
2026-08-08 16:58:09 +0800

    chore: 停止跟踪 .astro 生成缓存（已在 .gitignore 中）

--- .astro/content-assets.mjs
+++
@@ -1,7 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_Jkj9t from "old1-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fold-1%2Fold-1.md";
-import __ASTRO_IMAGE_IMPORT_p1u1y from "./01.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F0510-1%2Fmaincontent.md";
-import __ASTRO_IMAGE_IMPORT_Z1d7VEB from "./02.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F0510-1%2Fmaincontent.md";
-import __ASTRO_IMAGE_IMPORT_Z1Tqj4O from "./03.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F0510-1%2Fmaincontent.md";
-export default new Map([["old1-1.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fold-1%2Fold-1.md", __ASTRO_IMAGE_IMPORT_Jkj9t], ["./01.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F0510-1%2Fmaincontent.md", __ASTRO_IMAGE_IMPORT_p1u1y], ["./02.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F0510-1%2Fmaincontent.md", __ASTRO_IMAGE_IMPORT_Z1d7VEB], ["./03.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2F0510-1%2Fmaincontent.md", __ASTRO_IMAGE_IMPORT_Z1Tqj4O]]);
-		
